cf8916d8a812fe29bec20511d9f1a8f4b8be43ad
utkarshup11 <utkarshup11@gmail.com>
2021-03-15 13:30:16 +0530

    plugins added for scalastyle and scoverage for maven

--- src/main/scala/com/knoldus/CalculatorImpl.scala
+++ src/main/scala/com/knoldus/CalculatorImpl.scala
@@ -1,5 +1,20 @@
-
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.knoldus
+
 
 //A class to define all the methods of calculator
 class CalculatorImpl {
@@ -10,11 +25,11 @@ class CalculatorImpl {
   def multiply(firstNumber: Int, secondNumber: Int): Long = firstNumber * secondNumber
 
   def divide(numerator: Int, denominator: Int): Any = {
-    if (denominator == 0) {
-      "ArithmeticException : cant divide by 0"
-
-    } else
+    if (denominator != 0) {
       numerator / denominator
+    } else {
+      "ArithmeticException : cant divide by 0"
+    }
   }
 
 
@@ -22,24 +37,32 @@ class CalculatorImpl {
 
   def absolute(number: Int): Int = math.abs(number)
 
-  def modulus(firstNumber: Int, secondNumber: Int): Any =
-  {
-    if (secondNumber == 0) {
-      "ArithmeticException : cant divide by 0"
-
-    } else
+  def modulus(firstNumber: Int, secondNumber: Int): Any = {
+    if (secondNumber != 0) {
       firstNumber % secondNumber
+    } else {
+      "ArithmeticException : cant divide by 0"
+    }
+
   }
 
 
   def getMaximum(firstNumber: Int, secondNumber: Int): Int = {
-    if (firstNumber > secondNumber) firstNumber
-    else secondNumber
+    if (firstNumber > secondNumber) {
+      firstNumber
+    }
+    else {
+      secondNumber
+    }
   }
 
   def getMinimum(firstNumber: Int, secondNumber: Int): Int = {
-    if (firstNumber < secondNumber) firstNumber
-    else secondNumber
+    if (firstNumber < secondNumber) {
+      firstNumber
+    }
+    else {
+      secondNumber
+    }
   }
 
 }
